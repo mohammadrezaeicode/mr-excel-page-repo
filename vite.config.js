bb46a680a93defb7990a6989c6e74013dfdf0fb8
m.r <mr@example.com>
2023-08-11 12:36:16 -0800

    remove router

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/mr-excel-page/' : '/', // or '/', depending on your setup
   plugins: [vue()],
   resolve: {
+    // publicPath: '/', // or '/', depending on your setup
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
